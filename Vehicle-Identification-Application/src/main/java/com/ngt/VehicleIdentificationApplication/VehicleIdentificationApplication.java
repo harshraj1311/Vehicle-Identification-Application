c638ee3ee66a37061c7747143499ef9e348f2100
Palak3230 <palkueng3230@gmail.com>
2023-10-12 11:39:48 +0530

    first commit

---
+++ Vehicle-Identification-Application/src/main/java/com/ngt/VehicleIdentificationApplication/VehicleIdentificationApplication.java
@@ -0,0 +1,13 @@
+package com.ngt.VehicleIdentificationApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleIdentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleIdentificationApplication.class, args);
+	}
+
+}
---
+++ Vehicle-Identification-Application/src/test/java/com/ngt/VehicleIdentificationApplication/VehicleIdentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngt.VehicleIdentificationApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleIdentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
