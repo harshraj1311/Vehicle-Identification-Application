c284b05a3d6cc1622ecc6b0e97c185e339b7ad4c
Palak3230 <palkueng3230@gmail.com>
2023-10-12 11:23:09 +0530

    Initial commit

---
+++ VIp/src/main/java/com/nagarro/ngt/VIp/VIpApplication.java
@@ -0,0 +1,13 @@
+package com.nagarro.ngt.VIp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VIpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VIpApplication.class, args);
+		System.out.println("hello world!");
+	}
+}
---
+++ VIp/src/test/java/com/nagarro/ngt/VIp/VIpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.ngt.VIp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VIpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
